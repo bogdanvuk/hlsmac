9d133431c93ba2c2df40633df5b44d02085f0504
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2016-05-01 18:03:51 +0200

    1G MAC packing

--- script/mac1g_pack.py
+++ script/mac1g_pack.py
@@ -7,20 +7,30 @@ from pymake.builds.fileset import FilesetBuild, FileCopyBuild, FileBuild
 import os
 from pymake.utils import Fileset
 import itertools
+from pymake.builds.vivado import VivadoInteractInst, VivadoProject,\
+    VivadoIpProject, VivadoIpProjectBuild
+import time
 
-synths = Build(receive_synth, transmit_synth, axi_lite_synth)
-synths.srcs_setup['args'].target = 'synth'
-# fileset_combine = Build(*[FilesetBuild(match=[os.path.join('$BUILDDIR', name, 'solution1', 'syn', 'vhdl', '*.vhd')]) 
-#                           for name in ['axi_lite', 'receive', 'transmit']])
-
+synths = Build(receive_synth, transmit_synth, axi_lite_synth, FilesetBuild(match = ['$SRCDIR/module/mac1g_top/hdl/*.vhd']))
+synths.srcs_setup['args'].target = ['synth', 'synth', 'synth', 'all']
+ 
 class FilesetCombinedCopy(FileCopyBuild):
-    def build_postproc_args(self, name, res, collection, key):
-        if (collection=='') and (key[1] == 1):
-            return Fileset(list(itertools.chain(*res)), list_timestamp=min([f.list_timestamp for f in res]))
-        
-        return res
+    def build_postproc_src(self, name, res, collection, key):
+        return Fileset(list(itertools.chain(*res)), list_timestamp=min([f.list_timestamp for f in res]))
+ 
+hdl_fileset = FilesetCombinedCopy(synths, FileBuild('$BUILDDIR/ip/mac1g/hdl'))
+# i = VivadoInteractInst()
+# i.open()
+# resp = i.cmd('get_projects')
 
-fileset_combined_copy = FilesetCombinedCopy((FileBuild(os.path.join('..', '..', 'build', 'ip', 'mac1g', 'hdl')), synths),
-                                            env=dict(BUILDNAME="mac1g_pack", BUILDDIR='../../build', SRCDIR='../'))
-fileset_combined_copy.build()
-# b = FileCopyBuild((FileBuild(os.path.join('$BUILDDIR', 'ip', 'hdl')), b))
+#hdl = FilesetBuild(match=[os.path.join('..', '..', 'build', 'ip', 'mac1g', 'hdl/*.vhd')]).build()
+prj = VivadoIpProjectBuild(name     = 'mac1g_pack', 
+                           prjdir   = FileBuild('$BUILDDIR/mac1g_pack'), 
+                           ipdir    = FileBuild('$BUILDDIR/ip/mac1g'),
+                           sources  = {'sources_1': hdl_fileset},
+                           env      = dict(BUILDNAME="mac1g_pack", BUILDDIR='../../build', SRCDIR='../'))
+#prj.open()
+# prj.configure()
+# prj.close()
+prj.build()
+pass
